ac1a48988dc929429382942f7e40007b02c81bdc
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-05-17 17:20:15 +0430

    add library

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.convertorlibrary.Convert;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        double a  = Convert.convertFtoC(33.8);
+
     }
 }
