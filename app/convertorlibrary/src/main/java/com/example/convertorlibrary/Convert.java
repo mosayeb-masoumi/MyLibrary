9c5526e0e77399035ff17614276b6d0c97a41595
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-05-17 17:56:47 +0430

    add extra method to library

--- app/convertorlibrary/src/main/java/com/example/convertorlibrary/Convert.java
+++ app/convertorlibrary/src/main/java/com/example/convertorlibrary/Convert.java
@@ -2,7 +2,15 @@ package com.example.convertorlibrary;
 
 public class Convert {
 
+    // for v1.0
     public static double convertFtoC(double f ){
         return (f-32)/1.8;
     }
+
+
+   // for v1.0.1
+   public static String cleanWord(String string){
+        return  string.trim();
+   }
+
 }
--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 import com.example.convertorlibrary.Convert;
 
+//import com.example.convertorlibrary.Convert;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        double a  = Convert.convertFtoC(33.8);
+//        double a  = Convert.convertFtoC(33.8);
 
+           double a = Convert.convertFtoC(33.8);
+           double s = a;
     }
 }
